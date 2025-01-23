c814cbd9db194b6c65fe088144dbb7be8533bf72
Arni Magnusson <thisisarni@gmail.com>
2025-01-23 15:17:53 +1100

    Add haddock otolith dataset

---
+++ R/otoliths_had.R
@@ -0,0 +1,38 @@
+#' @docType data
+#'
+#' @name otoliths_had
+#'
+#' @title Otolith Data (Haddock)
+#'
+#' @description
+#' Otolith data for Icelandic haddock from the Icelandic spring bottom trawl
+#' survey 2010-2020.
+#'
+#' @usage
+#' otoliths_had
+#'
+#' @format
+#' Data frame containing two columns:
+#' \tabular{ll}{
+#'   \code{age} \tab age (years)\cr
+#'   \code{len} \tab length (cm)
+#' }
+#'
+#' @source
+#' Data contributed by the \href{https://www.hafogvatn.is/en/}{Icelandic Marine
+#' and Freshwater Research Institute}.
+#'
+#' @seealso
+#' \code{\link{gcm}}, \code{\link{gompertz}}\code{\link{gompertzo}},
+#' \code{\link{richards}}/\code{\link{richardso}}, \code{\link{schnute3}}, and
+#' \code{\link{vonbert}}/\code{\link{vonberto}} are alternative growth models.
+#'
+#' \code{otoliths_had}, \code{\link{otoliths_skj}}, and \code{\link{tags_skj}}
+#' are example datasets.
+#'
+#' \code{\link{fishgrowth-package}} gives an overview of the package.
+#'
+#' @examples
+#' head(otoliths_had)
+
+NA
