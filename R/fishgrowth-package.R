901b981b0a862de2d482ed11ec94b0cd2031d918
Arni Magnusson <thisisarni@gmail.com>
2025-01-28 03:17:19 +1100

    Expand package description

--- R/fishgrowth-package.R
+++ R/fishgrowth-package.R
@@ -5,11 +5,17 @@
 #' @title Fit Growth Curves to Fish Data
 #'
 #' @description
-#' Fit growth models to otoliths and/or tagging data, using RTMB and maximum
-#' likelihood. The otoliths provide direct observed coordinates of age and
-#' length. The tagging data provide information about the observed length at
-#' release and length at recapture at a later time, where the age at release is
-#' unknown and estimated as a vector of parameters.
+#' Fit growth models to otoliths and/or tagging data, using
+#' \code{\link[RTMB]{RTMB}} and maximum likelihood.
+#'
+#' The otoliths (or similar measurements of age) provide direct observed
+#' coordinates of age and length. The tagging data provide information about the
+#' observed length at release and length at recapture at a later time, where the
+#' age at release is unknown and estimated as a vector of parameters.
+#'
+#' The growth models provided by this package can be fitted to otoliths only,
+#' tagging data only, or a combination of the two. Growth variability can be
+#' modelled as constant or increasing with length.
 #'
 #' @details
 #' \emph{Growth models:}
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # fishgrowth
 
-Fit Growth Curves to Fish Data
+Fit growth models to otoliths and/or tagging data, using `RTMB` and maximum
+likelihood.
+
+The otoliths (or similar measurements of age) provide direct observed
+coordinates of age and length. The tagging data provide information about the
+observed length at release and length at recapture at a later time, where the
+age at release is unknown and estimated as a vector of parameters.
+
+The growth models provided by this package can be fitted to otoliths only (or
+other direct measurements of age), tagging data only, or a combination of the
+two. Growth variability can be modelled as constant or increasing with length.
